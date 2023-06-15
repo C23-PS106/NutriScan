c50571f9708663ecd1851982c69eec2acd8ea383
dwdw97 <dickywisnu10@gmail.com>
2023-06-16 06:36:32 +0700

    Ngasih permission internet

--- NutriScan/app/src/main/java/com/c23ps160/nutriscan/ResultActivity.kt
+++ NutriScan/app/src/main/java/com/c23ps160/nutriscan/ResultActivity.kt
@@ -17,6 +17,11 @@ class ResultActivity : AppCompatActivity() {
 
         val imageView: ImageView = findViewById(R.id.ivFoodImg)
         val tvFoodClass: TextView = findViewById(R.id.tvFoodName)
+        val textViewUkuranSaji = findViewById<TextView>(R.id.tvUkuranSaji)
+        val textViewKalori = findViewById<TextView>(R.id.tvKalori)
+        val textViewKarbohidrat = findViewById<TextView>(R.id.tvKarbohidrat)
+        val textViewLemak = findViewById<TextView>(R.id.tvLemak)
+        val textViewProtein = findViewById<TextView>(R.id.tvProtein)
 
         imageView.setImageBitmap(foodImg)
         tvFoodClass.text = foodName
